9a9986af451dfbfcdec6ae24df0f53dd70f324e9
達也林 <t_hayashi@hayashitatsuya-no-Mac-Pro.local>
2015-08-03 23:12:07 +0900

    Initial Commit

---
+++ Relux/Relux.h
@@ -0,0 +1,19 @@
+//
+//  Relux.h
+//  Relux
+//
+//  Created by 林達也 on 2015/08/03.
+//  Copyright © 2015年 jp.sora0077. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Relux.
+FOUNDATION_EXPORT double ReluxVersionNumber;
+
+//! Project version string for Relux.
+FOUNDATION_EXPORT const unsigned char ReluxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Relux/PublicHeader.h>
+
+
